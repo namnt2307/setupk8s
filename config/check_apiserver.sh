a5a3c544e401f0a370d2c48c64da5b01f0b4b552
xizibin <61072936+xizibin@users.noreply.github.com>
2020-12-29 13:30:41 +0700

    Update check_apiserver.sh

--- config/check_apiserver.sh
+++ config/check_apiserver.sh
@@ -6,6 +6,6 @@ errorExit() {
 }
 
 curl --silent --max-time 2 --insecure https://localhost:16443/ -o /dev/null || errorExit "Error GET https://localhost:16443/"
-if ip addr | grep -q ${APISERVER_VIP}; then
+if ip addr | grep -q 172.16.5.234 ; then
     curl --silent --max-time 2 --insecure https://172.16.5.234:16443/ -o /dev/null || errorExit "Error GET https://172.16.5.234:16443/"
 fi
